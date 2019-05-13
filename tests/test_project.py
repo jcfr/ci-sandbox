1dc34286f1fe2a62019fcfd16c66fd93ae535f27
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2019-05-13 14:58:13 -0400

    Add a test and enable coverage

---
+++ tests/test_project.py
@@ -0,0 +1,5 @@
+
+import pypi_upload_automation_sandbox as project
+
+def test_project_what():
+    assert project.what() == "This package is used only to test automation of upload to Python Package Index (PyPI)"
