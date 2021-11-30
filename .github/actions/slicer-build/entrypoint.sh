fcdb26dc624bf7c6d0d3041baa55bb70b043152e
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-11-30 10:24:17 -0500

    github-workflow: Debug passing of value between command

--- .github/actions/slicer-build/entrypoint.sh
+++ .github/actions/slicer-build/entrypoint.sh
@@ -4,10 +4,24 @@ set -x
 set -o pipefail
 set -o
 
-cp -r $GITHUB_WORKSPACE /usr/src/Slicer
+#cp -r $GITHUB_WORKSPACE /usr/src/Slicer
 
-/usr/src/Slicer-build/BuildSlicer.sh
-package=$(head -n1 /usr/src/Slicer-build/Slicer-build/PACKAGE_FILE.txt)
+echo "GITHUB_WORKSPACE [$GITHUB_WORKSPACE]"
+echo "RUNNER_TEMP [$RUNNER_TEMP]"
+
+#/usr/src/Slicer-build/BuildSlicer.sh
+#package_filepath=$(head -n1 /usr/src/Slicer-build/Slicer-build/PACKAGE_FILE.txt)
+slicer_dir="/usr/src/Slicer-build/Slicer-build"
+mkdir -p $slicer_dir
+
+package_filepath="$slicer_dir/Slicer-4.13.0-2021-11-30-linux-amd64.tar.gz"
+echo "Hello" > ${package_filepath}
+
+echo "package_filepath [${package_filepath}]"
+
+mv ${package_filepath} $RUNNER_TEMP/
+
+package=$(basename $package_filepath)
 echo "package [${package}]"
 
 echo "::set-output name=package::$package"
